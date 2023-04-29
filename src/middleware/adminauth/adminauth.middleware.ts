337d1f3838df06f0656c335a0036266cc79b3b92
longsiru <sirulong89@gmail.com>
2023-04-29 23:57:06 +0900

    Update adminauth.middleware.ts

--- src/middleware/adminauth/adminauth.middleware.ts
+++ src/middleware/adminauth/adminauth.middleware.ts
@@ -5,6 +5,7 @@ export class AdminauthMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
     console.log('middleware');
     console.log(req.originalUrl); //看看有没有打印出来路径，baseUrl: '/admin',originalUrl
+    var pathname = req.originalUrl;
     //1.获取session里面保存的用户信息。
     //2.避免进入死循环，要在if文里面排除掉adminlogin画面。所以需要获取URL传过来的值。如果是login就不需要进行权限判断。
     //3.拿到路径之后要进行排除。
