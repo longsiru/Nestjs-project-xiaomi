1f8bf7dd5e14b3c3749ade05a9c6bc1aacfe340d
longsiru <sirulong89@gmail.com>
2023-04-30 21:38:01 +0900

    Update admin.module.ts

--- src/module/admin/admin.module.ts
+++ src/module/admin/admin.module.ts
@@ -1,11 +1,19 @@
-//引入service
-import { ToolsService } from './../../service/tools/tools.service';
 import { Module } from '@nestjs/common';
 import { MainController } from './main/main.controller';
 import { LoginController } from './login/login.controller';
 import { ManagerController } from './manager/manager.controller';
+import { ToolsService } from '../../service/tools/tools.service';
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { AdminSchema } from '../../schema/admin.schema';
+//import {AdminService} from '../../service/admin/admin.service';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: 'Admin', schema: AdminSchema, collection: 'admin' },
+    ]),
+  ],
   controllers: [MainController, LoginController, ManagerController],
   providers: [ToolsService],
 })
