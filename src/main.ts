c208561d329937e28d7e81979c510dc10f65e9b5
longsiru <sirulong89@gmail.com>
2023-04-30 21:04:53 +0900

    mongoose

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@
 //20.局部刷新，用到iframe id="rightMain"，然后在a tag 里面写target="rightMain"
 //21.计算窗口高度，js
 //21. login生成验证码，用svg captcha  --npm install --save svg-captcha,安装完成之后引入 tools
-//22. 对登陆进行验证。没有login就不能进入。利用中间件来做，创建中间件。nest g mi middleware/adminauth
+//22. 对登陆进行验证。没有login就不能进入。可以利用守卫和中间件来做，这里我们创建中间件。nest g mi middleware/adminauth
 //23. 需要在跟模块配置中间件。需要继承nestmodule，实现config方法
 //24. 如何判断当前用户有没有登陆，要看用户信息有没有保存在session
 
