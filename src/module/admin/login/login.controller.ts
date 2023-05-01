3f8e4a1a512c553f8dbc9f1d9ab48c6f8090f83a
longsiru <sirulong89@gmail.com>
2023-05-01 23:23:11 +0900

    Update login.controller.ts

--- src/module/admin/login/login.controller.ts
+++ src/module/admin/login/login.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Render, Request, Response } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Post,
+  Render,
+  Req,
+  Request,
+  Response,
+} from '@nestjs/common';
 import { ToolsService } from '../../../service/tools/tools.service';
 import { AdminService } from '../../../service/admin/admin.service';
 
@@ -29,4 +38,79 @@ export class LoginController {
     //给页面返回一张图片
     res.send(svgCaptcha.data);
   }
+
+  @Post('doLogin')
+  //用bogy获取login ejs 传输过来的数据。
+  async doLogin(@Body() body, @Request() req, @Response() res) {
+    try {
+      var code: string = body.code;
+      var username: string = body.username;
+      var password: string = body.password;
+      if (username == '' || password.length < 6) {
+        console.log('username or password is wrong');
+        res.redirect('/admin/main');
+      } else {
+        console.log(req.session.code, code);
+        if (code.toUpperCase() == req.session.code.toUpperCase()) {
+          password = this.toolsService.getMd5(password); //加密密码
+          var userResult = await this.adminService.find({
+            username: username,
+            password: password,
+          });
+          if (userResult.length > 0) {
+            console.log('successfully');
+            req.session.userinfo = userResult[0];
+            res.redirect('/admin/main');
+          } else {
+            console.log('username or passwor is wrong');
+            res.redirect('/admin/login');
+          }
+        } else {
+          console.log(' code is wrong');
+          res.redirect('/admin/login');
+        }
+      }
+    } catch (error) {
+      console.log(error);
+      res.redirect('/admin/login');
+    }
+  }
+
+  /*
+   
+     注意：1、需要在前端页面用js验证用户输入的信息是否正确     2、后台获取数据以后判断数据格式是否正确
+
+
+
+     1、获取表单提交的数据 
+
+     2、判断验证码是否正确
+
+     验证码正确
+
+         1、要对表单里面的密码进行md5加密               md5模块  https://www.npmjs.com/package/md5
+
+                                                           1、安装 cnpm install md5 --save
+
+                                                           2、引入 md5 var md5 = require('md5');
+
+                                                           3、使用  md5(str)
+
+
+         2、在用户表（集合）中查询当前用户是否存在              （mongoose操作mongodb数据库）https://docs.nestjs.com/techniques/mongodb
+
+                                                                 1、配置mongoose
+
+                                                                 2、创建操作数据库的model
+
+
+         3、如果数据库有此用户（登录成功） ：保存用户信息     跳转到后台管理系统
+
+         4、如果数据库有此用户（登录失败）： 跳转到登录页面
+
+
+      验证码错误： 跳转到登录页面   提示验证码不正确
+
+
+     */
 }
