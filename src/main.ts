b10e756a6e9584046c381b348fcbf5967591055f
longsiru <sirulong89@gmail.com>
2023-05-03 00:42:57 +0900

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -63,7 +63,7 @@ async function bootstrap() {
 }
 bootstrap();
 
-//user login
+//30.user login
 //1.把输入表格的数据传输到后台。传输到后台之后首先验证验证码是否输入正确，如果验证码正确，
 //2.然后获取用户名和密码，然后再去数据库查询是否有匹配的用户名和密码。如果匹配就登录成功。
 //3.用到mongodb。需要安装nestjs/mongoose以及mongoose模块。npm install --save @nestjs/mongoose mongoose
@@ -78,4 +78,4 @@ bootstrap();
 //11.login.ejs --form action="/admin/login/doLogin"
 //12.login.controller @post
 
-//nestjs 实现登陆，退出登录功能，配置公共的成功，失败页面。
+//32.nestjs 实现登陆，退出登录功能，配置公共的成功，失败页面。
