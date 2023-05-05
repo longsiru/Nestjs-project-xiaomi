ecbe7cbbeadbc8b3630c9c1f646227f187b2a91e
longsiru <sirulong89@gmail.com>
2023-05-06 00:38:56 +0900

    Update init.middleware.ts

--- src/middleware/init/init.middleware.ts
+++ src/middleware/init/init.middleware.ts
@@ -2,10 +2,12 @@ import { Injectable, NestMiddleware } from '@nestjs/common';
 //建立好了中间件要在跟模块进行配置。
 //1.引入config
 import { Config } from '../../config/config';
+import { Helper } from '../../extend/helper';
 @Injectable()
 export class InitMiddleware implements NestMiddleware {
   use(req: any, res: any, next: () => void) {
     res.locals.config = Config;
+    res.locals.helper = Helper; //这样就可以在模版里面使用help的实例了
     next();
   }
 }
