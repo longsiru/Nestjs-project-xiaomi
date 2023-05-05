7a190067934e6e5698a7cbb473caa8f2962e94ea
longsiru <sirulong89@gmail.com>
2023-05-06 00:21:46 +0900

    Update app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,12 @@ import { DefaultModule } from './module/default/default.module';
 import { ApiModule } from './module/api/api.module';
 //配置loginauth的中间件,访问admin下面的页面都要在这个中间件进行权限判断
 import { AdminauthMiddleware } from './middleware/adminauth/adminauth.middleware';
+import { InitMiddleware } from './middleware/init/init.middleware';
 //connect db
 import { MongooseModule } from '@nestjs/mongoose';
 import { AdminService } from './service/admin/admin.service';
+//这里面也有url的设置所以也引入config
+import { Config } from './config/config';
 @Module({
   imports: [
     AdminModule,
@@ -21,6 +24,10 @@ import { AdminService } from './service/admin/admin.service';
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
-    consumer.apply(AdminauthMiddleware).forRoutes('admin/');
+    consumer
+      .apply(AdminauthMiddleware)
+      .forRoutes(`${Config.adminPath}/*`)
+      .apply(InitMiddleware)
+      .forRoutes('*'); //*应用于全部。所有的路由都要经过init中间件，这样的话config就有值了。admin就可以写成<%=config.adminPath%>了
   }
 }
